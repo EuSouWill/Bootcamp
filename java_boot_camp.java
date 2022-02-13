69a0349ce595a173bcfd7e22bbd177d2585a01f6
EuSouWIll <williamnunesregis@icloud.com>
2022-02-13 12:13:31 -0300

    BootCamp da Dio

---
+++ java_boot_camp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class java_boot_camp {
+
+    public static void main(String[] args) {
+        
+        int numero = 2;
+
+        for(numero =0; numero <3; numero++) {
+
+            System.out.println("seu numero está no vetor!");
+        }
+
+        System.out.println(numero);
+
+
+    }   
+
+
+}
